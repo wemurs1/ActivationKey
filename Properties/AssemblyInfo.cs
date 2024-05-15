6c6651ab2fd80b8d1caa2ce17c40531bc15aa3d7
Stuart Murray <stuart.murray.dev@outlook.com>
2024-05-15 12:31:12 +0100

    init

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Activation Key Library 2.0")]
+[assembly: AssemblyDescription("Activation Key 2.0 Library")]
+[assembly: AssemblyCompany("NG256")] // Developer.
+[assembly: AssemblyProduct("NG256 Activation Key")]
+[assembly: AssemblyCopyright("© NG256 2021-2024")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion("2.0.2405.0009")]
+[assembly: AssemblyFileVersion("2.0.2405.0009")]
+[assembly: AssemblyConfiguration("Release")]
+[assembly: Guid("28cdc5b3-3a32-413f-80ad-1a68da1871c8")]
+[assembly: ComVisible(false)]
